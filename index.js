edb5d74c90c4a9cbce4deae57250d1049b44bae9
alikurniawan <alikoerniawan02@gmail.com>
2020-10-24 00:09:18 +0700

    update

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const http = require("http");
 const { resourceUsage } = require("process");
+const fs = require("fs");
 
 const server = http.createServer((req, res) => {
   if (req.url === "/") {
     res.writeHead(200, { "content-type": "text/html" });
-    res.write("<h1>OK Youre In !!</h1>");
+    // res.write("<h1>OK Youre In !!</h1>");
+    const homePageHTML = fs.readFileSync("apps.html");
+    res.write(homePageHTML);
     res.end();
-  }else{
+  } else {
     res.writeHead(200, { "content-type": "text/html" });
     res.write("<h1>NO Youre NOT In !!</h1>");
     res.end();
